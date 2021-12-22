7413a65fc1cb0f5d5c77edb4141bf048b5ba84aa
DesignC <znehraks@gmail.com>
2021-12-22 21:59:49 +0900

    https...

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "http://bulletinboard-backend.herokuapp.com/",
+  baseURL: "https://bulletinboard-backend.herokuapp.com/",
 });
 
 export const Api = {
